77b23ec330612cd0bb05e713badcb95558647fa4
Ash <ashlegitimate@outlook.com>
2024-11-13 16:31:21 -0500

    Just applying input and text to prac

---
+++ SomeIntrostuff/BasicMadlibs.py
@@ -0,0 +1,12 @@
+#madlibs game 
+
+adj=input("Enter an adj:")
+noun = input("Enter noun:")
+adj2=input("Enter an adj2:")
+verb= input("Enter a verb with ing:")
+adj3=input("Enter an adj3:")
+
+print(f"Tomorrow I will go to a {adj} location")
+print(f"In the wild I saw a {noun}")
+print(f"The {noun} was {adj2} and {verb}")
+print(f"I was {adj3}!")
