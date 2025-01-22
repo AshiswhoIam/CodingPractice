0a6bb1ab9316abc8ee8dff35abeb0ab8d079688d
Ash <ashlegitimate@outlook.com>
2025-01-22 17:52:22 -0500

    Modules refresher

---
+++ SomeIntrostuff/Modules.py
@@ -0,0 +1,18 @@
+#skipped switch to modules. mod=> file with code u import...
+
+
+
+#import math
+#import math as m
+#from math import pi
+#print(pi)
+
+import Exmod
+
+#res=Exmod.pi
+#res=Exmod.square(3)
+#res=Exmod.cube(3)
+#res=Exmod.cir(3)
+res=Exmod.area(3)
+
+print(res)
