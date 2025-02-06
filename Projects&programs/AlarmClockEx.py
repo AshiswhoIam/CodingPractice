8cba9273ec459096e714cdb0d0f7f65c8f9a8ac5
Ash <ashlegitimate@outlook.com>
2025-02-06 14:08:50 -0500

    Alarm program

--- Projects&programs/AlarmClockEx.py
+++ Projects&programs/AlarmClockEx.py
@@ -6,7 +6,25 @@ import pygame
 
 def set_alarm(alarm_time):
     print(f"Alarm is set for {alarm_time}")
-    sound_file= "Hope.mp3"
+    sound_file= "Projects&programs\\Hope.mp3"
+    is_running = True
+    while is_running:
+        current_time=datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        if current_time == alarm_time:
+            print("It's time to wake up! 🔥🔥🔥")
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+            
+            is_running= False
+        
+        time.sleep(1)
+
+#AlarmClockEx
 if __name__=="__main__":
-    alarm_time = input("Enter the alarm time in hours,mins and seconds(00:00:00)")
+    alarm_time = input("Enter the alarm time in hours,mins and seconds(00:00:00): ")
     set_alarm(alarm_time)
